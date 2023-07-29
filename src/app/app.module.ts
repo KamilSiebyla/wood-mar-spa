222b04abbdcb58e9171e64184b6f533bd8d65159
Kamil Siebyła <kamil.siebyla@gmail.com>
2023-07-29 16:25:40 +0200

    Added some new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import {MatTabsModule} from '@angular/material/tabs';
 import {MatGridListModule} from '@angular/material/grid-list';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { 
   GridComponent, 
-  TabGroupComponent 
+  TabGroupComponent,
+  NavBarComponent
 } from 'src/components';
 
 @NgModule({
   declarations: [
     AppComponent,
     GridComponent,
-    TabGroupComponent
+    TabGroupComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatTabsModule,
-    MatGridListModule
+    MatGridListModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export * from './grid-component/grid-component';
 export * from './tab-group-component/tab-group-component';
+export * from './nav-bar-component/nav-bar-component';
